c7c3539834653720f42e54bae4930aa789a42afb
Aidan <aidands@protonmail.com>
2023-04-29 17:32:16 +0200

    Added challenge API endpoints

--- webapp/api.py
+++ webapp/api.py
@@ -9,5 +9,15 @@ import webapp.database as db
 
 
 @app.route('/api/get_users')
-def get_users():
+def api_get_users():
     return db.get_users()
+
+
+@app.route('/api/challenges/categories')
+def api_get_categories():
+    return db.get_categories()
+
+
+@app.route('/api/challenges/<category>')
+def api_get_challenges(category):
+    return db.get_challenges(category)
