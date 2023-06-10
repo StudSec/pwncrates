5ac10bf86b515c53ce577021434be8f80f20a240
Aidan <aidands@protonmail.com>
2023-06-10 18:32:37 +0200

    Switched to Secure & Same-Site=Strict cookies

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,5 +3,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
 
 from webapp import views, api, auth, template_preprocessor, database, git, mail
