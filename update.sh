af5d8200a79b65044fe305c8cf74ba0d4b812ce5
Aidan <aidands@protonmail.com>
2023-06-27 21:31:30 +0200

    Added update.sh, can be used to update the project

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run script from the pwncrates root directory
+cd $(dirname $0)
+
+git remote update >/dev/null 2>&1
+LOCAL=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse @{u})
+
+if [[ "$LOCAL" == "$REMOTE" ]]; then
+  echo "No updates available."
+  exit 0
+fi
+
+# Fetch the updates and reset to the latest commit
+git pull origin main
+
+# Execute the deployment command
+docker-compose down
+docker-compose build
+docker-compose up -d
