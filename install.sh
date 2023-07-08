864910d97fdc4aa3effa59f3e7956a9865113d28
Aidan <aidands@protonmail.com>
2023-07-08 12:14:54 +0200

    Minor update to install script

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ mkdir data/challenges 2>/dev/null
 if [[ $response == "y" ]]; then
   python3 scripts/test-users.py
   openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 -subj "/C=GB/ST=London/L=London/O=Alros/OU=IT Department/CN=localhost"
-  mv key.pem data/ssl
-  mv cert.pem data/ssl
+  mv key.pem data/ssl/
+  mv cert.pem data/ssl/
   git clone https://github.com/StudSec/Challenges-Examples.git
   mv Challenges-Examples data/challenges/Challenges
   echo "Test data populated!"
