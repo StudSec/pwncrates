2dc00a62afee726feb8635231540a4ebcab6c9cd
Aidan <aidands@protonmail.com>
2023-11-22 00:58:42 +0100

    Refactored codebase to work with CSP

--- webapp/template_preprocessor.py
+++ webapp/template_preprocessor.py
@@ -17,13 +17,20 @@ with open("config.json", "r") as f:
 def add_security_headers(resp):
     bootstrap_js_url = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
     bootstrap_css_url = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+
+    # Graph things
+    chart_url = "https://cdn.jsdelivr.net/npm/chart.js"
+    graph_url = "https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"
     resp.headers['Content-Security-Policy'] = ("default-src 'none'; "
                                                f"script-src {config['hostname'] + '/static/script.js'} "
-                                               f"{bootstrap_js_url}; "
+                                               f"{bootstrap_js_url} "
+                                               f"{chart_url} "
+                                               f"{graph_url}; "
                                                f"style-src {config['hostname'] + '/static/style.css'} "
                                                f"{bootstrap_css_url}; "
                                                "base-uri 'self'; "
-                                               "img-src *;")
+                                               "img-src * data:; "
+                                               "connect-src 'self';")
     return resp
 
 
