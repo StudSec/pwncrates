c213004e5a8d006dffb6d066b7f0b740a4fd35cc
Aidan <aidands@protonmail.com>
2023-12-21 14:54:04 +0100

    Updated test-user script.

--- scripts/test-users.py
+++ scripts/test-users.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+import sys
+
+os.chdir(sys.path[0])
 
 sql_statement = """
 INSERT INTO users
@@ -8,10 +12,11 @@ VALUES
     ('test_user_bravo', 'bravo@example.com', '$2b$12$v/vd5XM/WLLpMGXeKfrg0u7zd2AcJwQX7NNwSpZs4MXCZ7kYUlvr6'); -- the word yellow hashed --
 """
 
-with open('webapp/init.sql', 'r') as f:
+with open('../webapp/database/init.sql', 'r') as f:
     sql_code = f.read()
 
-conn = sqlite3.connect('data/db/pwncrates.db')
-conn.executescript(sql_code)
+conn = sqlite3.connect('../data/db/pwncrates.db')
+if os.path.getsize("../data/db/pwncrates.db") == 0:
+    conn.executescript(sql_code)
 conn.execute(sql_statement, ())
 conn.commit()
