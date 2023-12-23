db9614be1e143b46132e353730e52a3be8bca2c6
Aidan <aidands@protonmail.com>
2023-12-23 14:16:15 +0100

    Switched SameSite from Strict to Lax

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,12 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['SESSION_COOKIE_SECURE'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'
+# We have to put this at lax instead of secure to support Discord OAuth
+# Should we drop support for the "state" parameter in the OAuth flow this can go back on strict
+# However, as it stands we set the state parameter, navigate to Discord and navigate back to check if the
+# state parameters still match. This requires us to persist the session after exiting Discord, which is not
+# done in Strict.
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
