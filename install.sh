f1fee771c2991de8195f8ffc929ea118ac14bf18
delta <delta6862@protonmail.com>
2024-05-18 01:47:50 +0200

    Increased flexibility of adding new users from the cli

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ mkdir data/ssl 2>/dev/null
 mkdir data/db 2>/dev/null
 
 if [[ $response == "y" ]]; then
-  python3 scripts/test-users.py
+  python3 scripts/populate-users.py
   openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 -subj "/C=GB/ST=London/L=London/O=Alros/OU=IT Department/CN=localhost"
   mv key.pem data/ssl/
   mv cert.pem data/ssl/
---
+++ scripts/populate-users.py
@@ -0,0 +1,55 @@
+import sqlite3
+import secrets
+import random
+import bcrypt
+import csv
+import os
+import sys
+
+os.chdir(sys.path[0])
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+nato = [
+    "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India",
+    "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo",
+    "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"
+]
+with open("words.txt", "r") as f:
+    words = f.read().replace("'", "").splitlines()
+
+
+def register_user(user_name, password, email):
+    cursor = conn.execute('INSERT INTO users (name, password, email) VALUES (?, ?, ?)',
+                          (user_name, password, email))
+    conn.commit()
+    cursor.close()
+    return
+
+
+with open('../webapp/database/init.sql', 'r') as f:
+    sql_code = f.read()
+
+conn = sqlite3.connect('../data/db/pwncrates.db')
+if os.path.getsize("../data/db/pwncrates.db") == 0:
+    conn.executescript(sql_code)
+
+with open('users.csv') as csvfile:
+    users = csv.reader(csvfile, delimiter=',')
+    for user in users:
+        username = user[0]
+        password = user[1]
+        email = user[2]
+
+        if not username:
+            username = "-".join([random.choice(list(nato)) for _ in range(3)])
+        if not password:
+            password = "".join([secrets.choice(words) for _ in range(4)]).lower()
+        if not email:
+            email = "".join([random.choice(list(alphabet)) for _ in range(10)]) + "@example.com"
+        print("Adding:", username, password, email)
+        try:
+            register_user(username, bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode('ascii'), email)
+        except sqlite3.IntegrityError:
+            pass
+
+conn.commit()
