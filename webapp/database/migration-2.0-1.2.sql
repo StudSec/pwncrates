3b1ba9e53b81e2e12e490bf0bc34a9e62a87e59b
Thomas Andreatta <thomas.andreatta2000@gmail.com>
2025-02-01 18:33:56 +0100

    migration script to add admins table

---
+++ webapp/database/migration-2.0-1.2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS pwncrates (
+    name VARCHAR(64),
+    version FLOAT
+);
+
+UPDATE pwncrates SET version = 1.2 WHERE version = 1.1 AND name = 'pwncrates';
+
+CREATE TABLE admins (
+    user_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- need to remove this, just for testing purpose
+INSERT into admins values(2);
