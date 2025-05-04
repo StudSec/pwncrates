903892d759106e6954c1be6fd6093c021f247025
Aidan <aidands@protonmail.com>
2025-05-04 10:25:21 +0200

    Moved toml parsing to challenge object

---
+++ pwncrates/challenges.py
@@ -0,0 +1,119 @@
+from pwncrates import app
+import toml
+import os
+
+
+class Challenge:
+    def __init__(self, path, uuid):
+        self.path = path
+        config = toml.load(path + "/challenge.toml")
+        self.name = config[uuid]["name"]
+        self.uuid = uuid
+        self.difficulty = config[uuid]["difficulty"]
+        self.flag = config[uuid]["flag"]
+        self.url = config[uuid]["url"]
+        self.dynamic_flags = config.get("dynamic_flags", False)
+        self.handouts = []
+        self.category = None
+        self.description = ""
+
+        # Runtime variables
+        self.solves = []
+
+        if os.path.exists(path + "/Description.md"):
+            with open(self.path + "/Description.md") as f:
+                self.description += f.read()
+
+        self.port = None
+        if os.path.exists(self.path + "/Source/run.sh") or os.path.exists(self.path + "/Source/destroy.sh"):
+            self.hosted = True
+        else:
+            self.hosted = False
+
+        for dirpath, dirnames, filenames in os.walk(path + "/Handout"):
+            for filename in filenames:
+                relative_path = os.path.relpath(str(os.path.join(dirpath, filename)), path + "/Handout")
+                self.handouts.append(relative_path)
+
+    def allocate_port(self, generator):
+        if self.hosted:
+            self.port = str(next(generator))
+
+
+class Category:
+    def __init__(self, path):
+        self.path = path
+        config = toml.load(path + "/category.toml")
+        self.challenges = []
+        self.subcategories = []
+        self.parent = None
+        self.uuid = config["uuid"]
+        self.banner = config.get("banner", "")
+        self.name = config["name"]
+        self.description = ""
+
+        if os.path.exists(path + "/Description.md"):
+            with open(path + "/Description.md", "r") as f:
+                self.description += f.read()
+
+
+class ChallengeSet:
+    def allocate_ports(self):
+        # Allocate port in order of uuid
+        allocate_port = allocate_port_generator()
+        allocated_ports = {}
+        for uuid in sorted(self.challenges.keys()):
+            if self.challenges[uuid].path in allocated_ports.keys():
+                self.challenges[uuid].port = allocated_ports[self.challenges[uuid].path]
+            else:
+                self.challenges[uuid].allocate_port(allocate_port)
+                if self.challenges[uuid].port:
+                    allocated_ports[self.challenges[uuid].path] = self.challenges[uuid].port
+
+    def __init__(self, path: str):
+        self.challenges = {}
+        self.categories = {}
+
+        for dirpath, dirnames, filenames in os.walk(path):
+
+            # We don't want to try to parse challenge source, though this might be a bit overly aggressive
+            if any(folder in dirpath for folder in ["/Source/", "/Handout/", "/Tests/"]):
+                continue
+            try:
+                if "challenge.toml" in filenames:
+                    uuids = toml.load(dirpath + "/challenge.toml").keys()
+                    for uuid in uuids:
+                        if uuid in self.challenges.keys() or uuid in self.categories.keys():
+                            app.logger.warning(f"Duplicate uuid found: {uuid}", "red")
+                            continue
+
+                        self.challenges[uuid] = Challenge(dirpath, uuid)
+
+                        # Link to category
+                        category_uuid = toml.load(dirpath + "/../category.toml")["uuid"]
+                        self.categories[category_uuid].challenges.append(self.challenges[uuid])
+                        self.challenges[uuid].category = category_uuid
+                if "category.toml" in filenames:
+                    uuid = toml.load(dirpath + "/category.toml")["uuid"]
+                    if uuid in self.challenges.keys() or uuid in self.categories.keys():
+                        app.logger.warning(f"Warning: Duplicate uuid found: {uuid}")
+
+                    self.categories[uuid] = Category(dirpath)
+
+                    # Link to upper category, if exists
+                    if os.path.isfile(dirpath + "/../category.toml"):
+                        category_uuid = toml.load(dirpath + "/../category.toml")["uuid"]
+                        self.categories[category_uuid].subcategories.append(self.categories[uuid])
+                        self.categories[uuid].parent = category_uuid
+
+            except Exception as e:
+                app.logger.error(f"Error with {dirpath}: {e}", "red")
+
+        self.allocate_ports()
+
+def allocate_port_generator():
+    current = 4000
+    while current < 5000:
+        yield current
+        current += 1
+    raise Exception("Exhausted all ports.")
--- pwncrates/helpers.py
+++ pwncrates/helpers.py
@@ -7,7 +7,7 @@ import cmarkgfm
 import hashlib
 import sys
 import os
-import tomli
+import tomllib
 from webapp.helpers import *
 import webapp.database as db
 from webapp import app
@@ -32,157 +32,12 @@ def render_markdown(file_name, title=""):
     return render_template("markdown_page.html", markdown_content=content, page_title=title)
 
 
-# Iterate through challenge README.md, return title, description, points, flag
-def parse_markdown_challenge(path):
-    ret = {"url": "Null"}
-    with open(path, "r") as f:
-        lines = f.readlines()
-
-    try:
-        ret["description"] = ''.join(isolate_markdown_category(lines, "## Description\n"))
-        challenge_information = isolate_markdown_category(lines, "## Challenge information\n")
-
-        if not challenge_information:
-            raise ValueError
-
-        for line in challenge_information:
-            try:
-                key = line.split("|")[1].strip().lower()
-            except IndexError:
-                continue
-            # We want to skip over the ------ line
-            if key != len(key) * key[0]:
-                ret[key] = line.split("|")[2].strip()
-    except ValueError:
-        pass
-
-    if "subcategory" not in ret.keys():
-        ret["subcategory"] = ""
-
-    if "docker_name" not in ret.keys():
-        ret["docker_name"] = ""
-
-    if "case_insensitive" not in ret.keys():
-        ret["case_insensitive"] = False
-    elif "false" not in ret["case_insensitive"].lower():
-        # If the flag is present, we will assume it was intended to be case-insensitive unless explicitly
-        # directed otherwise.
-        ret["case_insensitive"] = True
-
-    if not all(x in ret.keys() for x in ["description", "flag", "points", "subcategory", "difficulty"]):
-        print(f"{path} doesn't contain all required challenge information, skipping", file=sys.stderr)
-        return {}
-
-    return ret
-
-import tomli  # or use tomllib for Python 3.11+
-import sys
-from typing import Dict, List, Any
-
-def parse_toml_challenge(path):
-
-    challenges = []
-    
-    try:
-        # Read and parse the TOML file
-        with open(path, "rb") as f:
-            toml_data = tomli.load(f)
-        
-        for challenge_id, challenge_data in toml_data.items():
-            ret = {"url": "Null"}
-            
-            # Set description if available, otherwise empty string
-            ret["description"] = challenge_data.get("description", "")
-            
-            # Handle flag field which could be a dictionary with flag-to-points mapping
-            if "flag" in challenge_data:
-                flag_data = challenge_data["flag"]
-                if isinstance(flag_data, dict):
-                    ret["flag"] = next(iter(flag_data.keys()))
-                    ret["points"] = next(iter(flag_data.values()))
-                else:
-                    ret["flag"] = flag_data
-                    ret["points"] = challenge_data.get("points", 0)
-            
-            for field in ["name", "url"]:
-                if field in challenge_data:
-                    # Handle URL arrays by taking the first one
-                    if field == "url" and isinstance(challenge_data[field], list):
-                        ret[field] = challenge_data[field][0]
-                    else:
-                        ret[field] = challenge_data[field]
-                        
-            if "difficulty" in challenge_data:
-                difficulty = challenge_data["difficulty"].lower()
-                if difficulty in ["easy", "medium", "hard"]:
-                    ret["difficulty"] = difficulty
-                else:
-                    ret["difficulty"] = "easy"
-            else:
-                ret["difficulty"] = "easy"
-            
-            ret["subcategory"] = challenge_data.get("subcategory", "")
-            ret["docker_name"] = challenge_data.get("docker_name", "")
-            
-            case_insensitive = challenge_data.get("case_insensitive", False)
-            if isinstance(case_insensitive, str):
-                ret["case_insensitive"] = "false" not in case_insensitive.lower()
-            else:
-                ret["case_insensitive"] = bool(case_insensitive)
-            
-            ret["id"] = challenge_id
-            
-            # Check if all required fields are present
-            required_fields = ["description", "flag", "points", "subcategory", "difficulty"]
-            if all(field in ret for field in required_fields):
-                challenges.append(ret)
-            else:
-                missing = [field for field in required_fields if field not in ret]
-                print(f"Challenge in {path} missing required fields: {missing}, skipping", file=sys.stderr)
-    
-    except Exception as e:
-        print(f"Error parsing {path}: {e}", file=sys.stderr)
-        import traceback
-        traceback.print_exc(file=sys.stderr)
-    
-    return challenges
-
-# Usage example:
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        challenges = parse_toml_challenge(sys.argv[1])
-        for challenge in challenges:
-            print(challenge)
-    else:
-        print("Please provide a TOML file path")
-
-# Parses category markdown, extracts any subcategories and descriptions
-# Returns a dict of all (sub)category names and descriptions and the parent category as a string
-def parse_markdown_category(path):
-    ret = {}
-
-    with open(path, "r") as f:
-        lines = f.readlines()
-
-    subcategories = [line for line in lines if "#### " in line]
-    try:
-        for category in subcategories:
-            ret[category[5:].strip()] = "\n".join(isolate_markdown_category(lines, category))
-
-        # Get main category description
-        ret[path.split("/")[-2]] = "\n".join(isolate_markdown_category(lines, lines[0]))
-    except ValueError:
-        pass
-
-    return ret, path.split("/")[-2]
-
-
 # Create challenge zip in the static folder sha1(challenge_name + category).zip
 def create_challenge_handouts(path):
     category, name, _ = path.split("/", 2)
     challenge_path = get_challenge_path()
     # Replaces existing zip
+    # TODO: refactor to not use relative path
     subprocess.run(['zip', '-FSr', f'../../../../static/handouts/{get_handout_name(category, name)}',
                     f'Handout'],
                    stdout=subprocess.DEVNULL,
@@ -200,7 +55,7 @@ def isolate_markdown_category(lines, header):
     # Reached end of file
     return lines[lines.index(header) + 1:]
 
-
+# TODO: switch up to uuid
 def get_handout_name(category, name):
     zip_file = hashlib.sha1()
     zip_file.update((category + name).encode())
